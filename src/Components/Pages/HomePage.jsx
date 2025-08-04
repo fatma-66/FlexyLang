867fd6271645b50dcff2ea70904b5c57a7338db7
Fatma Ramadan <ramadanfatma66@gmail.com>
2025-08-04 09:47:55 +0200

    Create Home and welcome page

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import gurl from '../../assets/GIF/gurl.gif'
+import CoursesContent from '../Molecules/CoursesContent'
+import Divider from '../Atoms/Divider'
+
+
 function HomePage() {
     return (
         <>
-        
+     <div className=' py-5'>
+ <h1 className="text-[100px] text-orange-500 font-extrabold  animate__animated animate__bounceInDown p-10"  style={{ textShadow: '2px 2px 3px rgba(0,0,0,0.3)' }}>
+         <span className=""> Flexy{" "}</span>
+          <div className=" animate__animated animate__jackInTheBox  animate__repeat-1">
+            Language
+          </div>
+        </h1>
+     </div>
+
+     <CoursesContent/>
+   
         </>
     )
 }
---
+++ src/Components/Pages/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+/* import BeeGif from '/bee.mp4' */
+function WelcomePage() {
+  return (
+    <>
+      <div className="py-5 "></div>
+    </>
+  );
+}
+
+export default WelcomePage;
