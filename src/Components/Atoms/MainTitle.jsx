3e0e6aa8587beab405cccc8b5be2f3240c137a12
Fatma Ramadan <ramadanfatma66@gmail.com>
2025-08-24 09:37:05 +0200

    Add reusable title for all pages

--- src/Components/Atoms/MainTitle.jsx
+++ src/Components/Atoms/MainTitle.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function MainTitle({title,src}) {
-    return (
-      <>
-<div className='flex flex-row gap-3 items-center'>
-    <img src={src}  alt=''/>
-<h1>
-   {title} 
-</h1>
-</div>
-      </>  
-    )
+function MainTitle({ title, src, classes, icon }) {
+  return (
+    <>
+      <div className="flex flex-row items-center pt-10">
+        {src ? <img src={src} className={classes} /> : icon}
+
+        <h1 className="text-orange-400 text-[50px] font-bold ">{title}</h1>
+      </div>
+    </>
+  );
 }
 
-export default MainTitle
+export default MainTitle;
